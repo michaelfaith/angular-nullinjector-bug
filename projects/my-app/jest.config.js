76c2d34eb4f81f6d23ca4372fb41f6790e11c8e5
michael faith <michael.faith@gm.com>
2023-07-18 07:58:17 -0500

    Remove unnecessary config

--- projects/my-app/jest.config.js
+++ projects/my-app/jest.config.js
@@ -7,16 +7,5 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/src/jest-setup.ts'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['html', 'json-summary', 'text', 'cobertura'],
-  coverageThreshold: {
-    global: {
-      statements: 90,
-      branches: 80,
-    },
-  },
-  modulePathIgnorePatterns: ['<rootDir>/projects/test-lib'],
-  reporters: ['default', 'jest-junit'],
-  testTimeout: 15000,
+  reporters: ['default'],
 };
